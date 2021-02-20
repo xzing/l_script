1e966466b719d24a7d4406b4ea9f606296f80119
Zing <pzl144@zcsmart.com>
2021-02-20 11:05:17 +0800

    多进程下载

--- python3/MultiYouGet.py
+++ python3/MultiYouGet.py
@@ -1,19 +1,42 @@
 # -*- coding: utf-8 -*-
 import os
+import sys
 import threading
+from multiprocessing import Pool
+from you_get import common as you_get
 
 
 # https://www.bilibili.com/video/BV1S7411m7vM?p=2
 def download():
-    for i in range(0,10):
-        urlStr = "https://www.bilibili.com/video/BV1FE411y79Y?p=%d" % i
+    #  串行
+    for i in range(40,60):
+        urlStr = "https://www.bilibili.com/video/BV1c4411J7Ty?p=%d" % i
         print(urlStr)
         myThread(i, "Thread-%d" % i, urlStr).start()
     pass
 
+def multi_process(urls):
+    sys.argv = ['you-get', '-o', save_to, '--no-caption', urls]
+    you_get.main()
+
+
+
+def multi_download(start, end):
+    urlStr = 'https://www.bilibili.com/video/BV1c4411J7Ty?p='
+    process_url=[]
+    for i in range(start, end):
+        process_url.append(urlStr + str(i))
+    print(urlStr)
+    print("Starting ...")
+    pool = Pool(8)
+    pool.map(multi_process, process_url)
+    pool.close()
+    pool.join()
+    print("wailting ..." )
+
 
 exitFlag = 0
-save_to = "G:\\TOAli1"
+save_to = "G:\\Netty"
 
 
 class myThread(threading.Thread):  # 继承父类threading.Thread
@@ -30,4 +53,5 @@ class myThread(threading.Thread):  # 继承父类threading.Thread
 
 
 if __name__ == '__main__':
-    download()
+    # download()
+    multi_download(60, 90)
